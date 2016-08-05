60b464ca9c667a17c1fb665824a566584d254355
egandalf <jstout@ITs-MacBook-Pro.local>
2016-08-05 10:45:32 -0400

    Initial Commit

---
+++ EditorDescriptors/TinyMcePlugins/ACheck.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Editor.TinyMCE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OxxCommerceStarterKit.Web.EditorDescriptors.TinyMcePlugins
+{
+    [TinyMCEPluginButton(
+        PlugInName = "acheck", 
+        //EditorInitConfigurationOptions = "{ accessibility_warnings: true }", 
+        ButtonName = "acheck", 
+        GroupName = "misc",
+        LanguagePath = "admin/tinymce/plugins/acheck", 
+        IconUrl = "Editor/tinymce/plugins/acheck/img/acheck.gif",
+        Description = "Accessibility Checker",
+        DisplayName = "Accessibility Check")]
+    public class ACheck
+    {
+    }
+}
